cce8588dc6ff6464e56b3a1ee22e4e54d4c68644
Andrew Cantrell <andrew.cantrell@keysight.com>
2025-04-15 14:46:31 -0700

    chore: initial project setup with frontend and backend

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
